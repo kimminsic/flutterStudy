fbe96665b23c08e7636a4a7dd25a18f18ee87b60
김민식 <alstlr1135@gmail.com>
2025-06-02 16:43:13 +0900

    Flutter로 웹툰 앱 만들기 #6.2 Data Fetching

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toolflix/screens/home_screen.dart';
+import 'package:toolflix/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const App());
 }
 
