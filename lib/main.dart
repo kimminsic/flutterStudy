7d12a6362671179fb13c38428105763d71e0064c
김민식 <alstlr1135@gmail.com>
2025-07-06 15:59:42 +0900

    Flutter로 웹툰 앱 만들기 #6.5 waitForWebToons

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:toolflix/screens/home_screen.dart';
 import 'package:toolflix/services/api_service.dart';
 
 void main() {
-  ApiService().getTodaysToons();
   runApp(const App());
 }
 
